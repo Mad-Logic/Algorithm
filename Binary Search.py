c6cb9c2e3854ab6208285945a1fb11ca62156491
Peter Chang <94877099+Mad-Logic@users.noreply.github.com>
2022-03-08 00:26:55 +0900

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,11 +1,11 @@
-#Binary Search Algorithm
-#Binary search works on sorted arrays. Binary search begins by comparing an element in the middle of the array with the target value. 
-#If the target value matches the element, its position in the array is returned. 
-#If the target value is less than the element, the search continues in the lower half of the array. 
-#If the target value is greater than the element, the search continues in the upper half of the array. 
-#By doing this, the algorithm eliminates the half in which the target value cannot lie in each iteration
-#Big O Time  = O(logN)
-#From WikiPedia 
+'''Binary Search Algorithm
+Binary search works on sorted arrays. Binary search begins by comparing an element in the middle of the array with the target value. 
+If the target value matches the element, its position in the array is returned. 
+If the target value is less than the element, the search continues in the lower half of the array. 
+If the target value is greater than the element, the search continues in the upper half of the array. 
+By doing this, the algorithm eliminates the half in which the target value cannot lie in each iteration
+Big O Time  = O(logN)
+From WikiPedia'''
 
 def binarySearch(array, value, low, high):
 	if low > high:
